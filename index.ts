967114372243edaaae46268d475348003c13658b
Dani <d.tarazoma@geekshubs.com>
2023-06-15 10:52:36 +0200

    feat: server running

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (__req: Request, res: Response) => {
+    return res.send('Welcome to my app')
+});
+
+app.get('/healthy', (__req: Request, res: Response) => {
+    return res.send('Healthy')
+});
+
+app.listen(PORT, () => {
+    console.log("Server running on port:" + PORT);
+})
